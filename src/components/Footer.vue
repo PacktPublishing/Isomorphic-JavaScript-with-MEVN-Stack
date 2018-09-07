adf701062df56ef8e270e2723b9d0994cb796349
David Acosta <gugui3z24@gmail.com>
2018-09-07 18:04:10 -0400

    added home component and styles

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="custom-footer">
+        <span class="mr-4">&copy; 2018 David Acosta</span>
+        <span class="mr-4">Private</span>
+        <span>Terms of Service</span>
+    </footer>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top custom-bg-dark">
+            <a class="navbar-brand" href="#">
+                <img style="max-height:25px;" src="../assets/logo.png" /> Task Manager
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tasks</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">David</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
