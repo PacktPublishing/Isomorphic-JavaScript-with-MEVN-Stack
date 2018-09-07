2af29237eb4242618ee5e95c6f1fefa6f0d063ad
David Acosta <gugui3z24@gmail.com>
2018-09-07 18:18:48 -0400

    added navigation guards

--- src/router.js
+++ src/router.js
@@ -9,6 +9,8 @@ import TasksEdit from './views/tasks/TasksEdit.vue'
 
 Vue.use(Router)
 
+const isLoggedIn = true; // Temporarily used to mark the user as logged in until AuthService is implemented
+
 export default new Router({
   routes: [
     {
@@ -19,27 +21,63 @@ export default new Router({
     {
       path: '/tasks',
       name: 'tasks-all',
-      component: TasksAll
+      component: TasksAll,
+      beforeEnter: (toolbar, from, next) => {
+        // Navigation Guard protects this route. User must be logged in, else will be routed to login page
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
+      }
     },
     {
       path: '/tasks/new',
       name: 'tasks-create',
-      component: TasksCreate
+      component: TasksCreate,
+      beforeEnter: (toolbar, from, next) => {
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
+      }
     },
     {
       path: '/tasks/:id',
       name: 'tasks-edit',
-      component: TasksEdit
+      component: TasksEdit,
+      beforeEnter: (toolbar, from, next) => {
+        if (isLoggedIn) {
+          next();
+        } else {
+          next('/login');
+        }
+      }
     },
     {
       path: '/register',
       name: 'register',
-      component: Register
+      component: Register,
+      beforeEnter: (toolbar, from, next) => {
+        if (!isLoggedIn) {
+          next();
+        } else {
+          next('/');
+        }
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      beforeEnter: (toolbar, from, next) => {
+        if (!isLoggedIn) {
+          next();
+        } else {
+          next('/');
+        }
+      }
     },
     {
       path: '*',
